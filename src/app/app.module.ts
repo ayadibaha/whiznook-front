25795d7d45270fcab4e921828a9df5bd2b081298
ayadibaha <ayadibaha51@gmail.com>
2019-03-22 12:07:17 +0100

    Landing page first design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TopSideComponent } from './top-side/top-side.component';
+import { FooterComponent } from './footer/footer.component';
+import { OffersComponent } from './offers/offers.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TopSideComponent,
+    FooterComponent,
+    OffersComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
